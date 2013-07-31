898ac2496caff34e426650e1874e90394e59824b
Florian Klinglmueller <float@lefant.net>
2013-07-31 14:56:58 +0200

    fixed load path

--- exportfunctions/examplegraphs.R
+++ exportfunctions/examplegraphs.R
@@ -1,7 +1,5 @@
 require(gMCP)
-
-source('~/newLife/code/gMCP/exportfunctions/functions.R')
-graphGUI()
+source('functions.R')
 
 e <- environment()
 sapply(c('full4.Rd','base4.Rd','cycle4.Rd','claw4.Rd'),load,env=e)
@@ -12,7 +10,7 @@ exportUDgraph(cycle4,file='cycle4.tex')
 exportUDgraph(base4,file='base4.tex')
 exportUDgraph(claw4,file='claw4.tex')
 
-
+## tikz does not work on modesim?
 system('pdflatex full4.tex') 
 system('pdflatex cycle4.tex')
 system('pdflatex base4.tex')
